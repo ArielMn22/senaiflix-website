aa9ea4771595a58847f26b7d2fddc3086684ca2d
ArielMn22 <arielpaixao10@gmail.com>
2024-12-05 16:26:17 -0300

    feat(ids): adding ids redirection.

---
+++ detalhes-do-filme/scripts.js
@@ -0,0 +1,13 @@
+function fecharCabecalhoMobile () {
+
+    let cabecalho = document.querySelector("#cabecalho-mobile");
+    cabecalho.style.display = "none";
+
+}
+
+function abrirCabecalhoMobile () {
+    
+    let cabecalho = document.querySelector("#cabecalho-mobile");
+    cabecalho.style.display = "block";
+
+}
