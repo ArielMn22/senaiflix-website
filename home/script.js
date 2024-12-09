c8d37f90b51e61bf6baca704dea27c397dabbb74
ArielMn22 <arielpaixao10@gmail.com>
2024-12-09 08:14:33 -0300

    feat(demo): demonstration

---
+++ home/script.js
@@ -0,0 +1,8 @@
+function fecharCabecalhoMobile () {
+
+    // Pega a referencia do elemento através do ID.
+    let cabecalhoMobile = document.querySelector("#cabecalho-mobile");
+
+    cabecalhoMobile.style.display = "none";
+
+}
